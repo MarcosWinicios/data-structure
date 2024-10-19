4b3b2f97ca7dde0d6c8bdf8bfa6ca5d6e05c221b
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-10-19 00:20:45 -0300

    feat(search): add linear search algorithm

--- src/main/java/com/studies/array/StringArray.java
+++ src/main/java/com/studies/array/StringArray.java
@@ -21,5 +21,35 @@ public class StringArray {
         for (int i = 0; i < states.length; i++) {
             System.out.println("Estado: " + states[i]);
         }
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Digite uma sigla de estado a ser buscada: ");
+
+        String acronymQuery = reader.nextLine();
+        linearSearch(acronymQuery, states);
+
+    }
+
+    /***
+     * busca linear: busca que caminha da primeira até a última posição do array
+     * É uma busca ineficiente, por ser lenta.
+     * A medida que o tamanho do array aumenta, o tempo de busca também aumenta
+     */
+    public static void linearSearch(String targetValue, String[] array) {
+        boolean isPresent = false;
+        int position = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equalsIgnoreCase(targetValue)) {
+                isPresent = true;
+                position = i;
+                break;
+            }
+        }
+
+        if (isPresent) {
+            System.out.println("Esse estado foi encontrado na posição: " + position);
+        } else {
+            System.out.println("Estado não encontrado");
+        }
     }
 }
