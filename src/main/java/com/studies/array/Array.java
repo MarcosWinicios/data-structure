5eac7fa1ce929412952c9875c2eaf76cf271b09b
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-10-18 23:53:06 -0300

    feat(array): add data structure array

---
+++ src/main/java/com/studies/array/Array.java
@@ -0,0 +1,29 @@
+package com.studies.array;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        array[0] = 3;
+        array[1] = 4;
+        array[3] = 13;
+        array[2] = 8;
+        array[5] = 51;
+        array[4] = 22;
+        array[6] = 33;
+
+        System.out.println("Impimindo...");
+        System.out.println(array[0]); //3
+        System.out.println(array[1]); //4
+        System.out.println(array[6]); //33
+        System.out.println(array[8]); //0 -> Posições não preenchidas assumem o valor 0 para arrays numéricos
+
+
+        System.out.println("Imprimir todos...");
+
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+
+    }
+}
