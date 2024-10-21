9696e8c20d5c292cba978779e7292dd63e9e6977
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-10-20 23:38:37 -0300

    feat(linked-list): add linkedList package With classes that represent the LinkedList data structure

---
+++ src/main/java/com/studies/linkedList/LinkedList.java
@@ -0,0 +1,33 @@
+package com.studies.linkedList;
+
+public class LinkedList {
+
+    private Node first;
+    private Node last;
+    private Integer size;
+
+    public Node getFirst() {
+        return first;
+    }
+
+    public void setFirst(Node first) {
+        this.first = first;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+
+    public void setLast(Node last) {
+        this.last = last;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+}
+
