f1a5dedff659c871695c8220a18f81288e69c367
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-10-21 23:59:39 -0300

    test(linked-list): Add remove method test in Linked List test class

--- src/main/java/com/studies/linkedList/Program.java
+++ src/main/java/com/studies/linkedList/Program.java
@@ -20,5 +20,19 @@ public class Program {
         System.out.println(list.get(1).getValue());
         System.out.println(list.get(2).getValue());
         System.out.println(list.get(3).getValue());
+
+        printLinkedList(list);
+
+        //remover estado CE
+        list.remove("CE");
+        System.out.println("removeu estado CE");
+        printLinkedList(list);
+    }
+
+    public static void printLinkedList(LinkedList list){
+
+        for(int i = 0; i < list.getSize(); i++){
+            System.out.println(list.get(i).getValue());
+        }
     }
 }
