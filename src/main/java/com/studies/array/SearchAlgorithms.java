c10e00621636cd4f85b683d06ca87717a834e3f2
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-10-28 23:34:59 -0300

    feat(search): add class to compare search algorithms

---
+++ src/main/java/com/studies/array/SearchAlgorithms.java
@@ -0,0 +1,45 @@
+package com.studies.array;
+
+import com.studies.utils.Utils;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SearchAlgorithms {
+
+    public static void main(String[] args) {
+        int[] array = new int[8];
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = (int) (Math.random() * 10);
+        }
+
+        Utils.printArray(array);
+
+        System.out.println("Qual numero você busca?");
+        Scanner reader = new Scanner(System.in);
+
+        int searchNumber = reader.nextInt();
+
+        linearSearch(searchNumber, array);
+
+    }
+
+    //O(n)
+    public static void linearSearch(int searchNumber, int[] array){
+        boolean isPresent = false;
+        int position = 0;
+        for(int i = 0; i < array.length;i++){
+            if(array[i] == searchNumber){
+                isPresent = true;
+                position = i;
+                break;
+            }
+        }
+        if(isPresent){
+            System.out.println("Elemento encontrado na posição: " + position);
+        }else{
+            System.out.println("Valor não encontrado");
+        }
+    }
+}
