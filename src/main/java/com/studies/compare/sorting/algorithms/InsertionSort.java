22334c7cfec601fb0ba1cce80cbe9b8cf158c75b
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-11-11 00:30:27 -0300

    feat(compare): add InsertionSort class

---
+++ src/main/java/com/studies/compare/sorting/algorithms/InsertionSort.java
@@ -0,0 +1,35 @@
+package com.studies.compare.sorting.algorithms;
+
+import com.studies.compare.sorting.Result;
+
+public class InsertionSort implements SortingAlgorithmsInterface{
+
+    private final String name =  "INSERTION SORT";
+    private final String message = "Ordenação utilizando o algoritmo " + name;
+
+    @Override
+    public Result sort(int[] array) {
+        int aux, j;
+        for (int i = 1; i < array.length; i++) {//O(N)
+            aux = array[i];
+            j = i - 1;
+            while (j >= 0 && array[j] > aux) {//O(N-1) -> O(N)
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = aux;
+        }
+
+        return new Result(array, 0);
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+}
