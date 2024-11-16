eaf8c60c56dcd27d9db1c555a12434916b972ab8
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-11-15 23:57:15 -0300

    feat(compare): add Shell Sort algorithm

--- src/main/java/com/studies/compare/sorting/Main.java
+++ src/main/java/com/studies/compare/sorting/Main.java
@@ -5,12 +5,14 @@ import com.studies.compare.sorting.algorithms.HeapSort;
 import com.studies.compare.sorting.algorithms.InsertionSort;
 import com.studies.compare.sorting.algorithms.QuickSort;
 import com.studies.compare.sorting.algorithms.SelectionSort;
+import com.studies.compare.sorting.algorithms.ShellSort;
 import com.studies.compare.sorting.algorithms.SortingAlgorithmsInterface;
 import com.studies.utils.Utils;
 
 public class Main {
 
-    private final static int[] DESORDERED_ARRAY = Utils.generateRandomIntegerArray(10000);
+//    private final static int[] DESORDERED_ARRAY = Utils.generateRandomIntegerArray(10);
+    private final static int[] DESORDERED_ARRAY = {2, 10, 4, 7, 1, 8, 9, 3, 6, 5};
 
     public static void main(String[] args) {
         SortingAlgorithmsInterface bubbleSort = new BubbleSort();
@@ -18,12 +20,14 @@ public class Main {
         SortingAlgorithmsInterface selectionSort = new SelectionSort();
         SortingAlgorithmsInterface heapSort = new HeapSort();
         SortingAlgorithmsInterface quickSort = new QuickSort();
+        SortingAlgorithmsInterface shellSort = new ShellSort();
 
         execute(bubbleSort, DESORDERED_ARRAY.clone());
         execute(insertionSort, DESORDERED_ARRAY.clone());
         execute(selectionSort, DESORDERED_ARRAY.clone());
         execute(heapSort, DESORDERED_ARRAY.clone());
         execute(quickSort, DESORDERED_ARRAY.clone());
+        execute(shellSort, DESORDERED_ARRAY.clone());
     }
 
     public static void execute(SortingAlgorithmsInterface sortInterface, int[] array){
---
+++ src/main/java/com/studies/compare/sorting/algorithms/ShellSort.java
@@ -0,0 +1,43 @@
+package com.studies.compare.sorting.algorithms;
+
+import com.studies.compare.sorting.Result;
+
+public class ShellSort implements SortingAlgorithmsInterface{
+
+    private final String name = "SHELL SORT";
+
+    @Override
+    public Result sort(int[] array) {
+        int h = 1;
+        int size = array.length;
+
+        while (h < size){
+            h = h * 3 + 1;
+        }
+
+        h = (int) Math.floor(h / 3);
+
+        int element, j;
+
+        while (h > 0){
+            for(int i = h; i < size; i++){
+                element = array[i];
+                j = i;
+
+                while (j >= h && array[j - h] > element){
+                    array[j] = array[j - h];
+                    j = j - h;
+                }
+                array[j] = element;
+            }
+            h = h /2;
+        }
+
+        return new Result(array, 0);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+}
