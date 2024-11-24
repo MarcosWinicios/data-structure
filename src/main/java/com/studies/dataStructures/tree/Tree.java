dc3bfd0c13c28dbfd919fa2d106b2c78ab35c250
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-11-24 06:03:52 -0300

    feat(tree): add method to add values in the Tree class

--- src/main/java/com/studies/dataStructures/tree/Tree.java
+++ src/main/java/com/studies/dataStructures/tree/Tree.java
@@ -9,4 +9,32 @@ public class Tree<Type extends Comparable> {
         this.root = null;
     }
 
+
+    public void add(Type value){
+        Element<Type> newElement =  new Element<>(value);
+
+        if(this.root == null){
+            this.root = newElement;
+        }else{
+            Element<Type> current =  this.root;
+            while (true){
+                if(newElement.getValue().compareTo(current.getValue()) < 0){
+                    if(current.getLeft() != null){
+                        current = current.getLeft();
+                    }else{
+                        current.setLeft(newElement);
+                        break;
+                    }
+                }else { //se o novo elemento for maior ou igual
+                    if(current.getRight() != null){
+                        current = current.getRight();
+                    }else{
+                        current.setRight(newElement);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
 }
