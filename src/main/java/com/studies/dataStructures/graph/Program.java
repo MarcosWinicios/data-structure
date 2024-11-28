bb12c8c61dab2293c7c601a1c7c2de5b4446d0a4
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-11-28 02:19:26 -0300

    test(graph): add graph test Class

---
+++ src/main/java/com/studies/dataStructures/graph/Program.java
@@ -0,0 +1,21 @@
+package com.studies.dataStructures.graph;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Graph<String> graph = new Graph<>();
+
+        graph.addVertex("João");
+        graph.addVertex("Lorenzo");
+        graph.addVertex("Creuza");
+        graph.addVertex("Créber");
+        graph.addVertex("Cráudio");
+
+        graph.addEdge(2.0, "João", "Lorenzo");
+        graph.addEdge(3.0, "Lorenzo", "Créber");
+        graph.addEdge(1.0, "Créber", "Creuza");
+        graph.addEdge(1.0, "João", "Creuza");
+        graph.addEdge(2.0, "Cráudio", "Lorenzo");
+        graph.addEdge(3.0, "Cráudio", "João");
+    }
+}
